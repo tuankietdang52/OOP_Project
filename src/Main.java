1c6b456146add3228ec025f29241749b7543a246
tuankietdang52 <dkiet27@gmail.com>
2023-11-04 20:12:05 +0700

    ah shiba

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+
+import InputManage.Input;
+import ProductContainer.Shirt;
+import Users.Customer;
+import Users.Employee;
+
+import java.util.Scanner;
+
+public class Main {
+    public final Scanner scanner = new Scanner(System.in);
+    public static void main(String[] args) {
+        Customer e = new Customer();
+        e.setByInput();
+
+        System.out.println(e);
+
+        Input.close();
+    }
+}
---
+++ src/Users/Customer.java
@@ -0,0 +1,98 @@
+package Users;
+
+import InputManage.Input;
+import org.jetbrains.annotations.NotNull;
+
+public class Customer {
+    private String makh;
+    private String tenkh;
+    private String diachi;
+    private String sdt;
+    private String email;
+
+    public Customer(){
+        makh = "";
+        tenkh = "";
+        diachi = "";
+        sdt = "";
+        email = "";
+    }
+
+    public Customer(String makh, String tenkh, String diachi, String sdt, String email){
+        this.makh = makh;
+        this.tenkh = tenkh;
+        this.diachi = diachi;
+        this.sdt = sdt;
+        this.email = email;
+    }
+
+    public Customer(@NotNull Customer customer){
+        makh = customer.makh;
+        tenkh = customer.tenkh;
+        diachi = customer.diachi;
+        sdt = customer.sdt;
+        email = customer.email;
+    }
+
+    public void setMakh(String makh) {
+        this.makh = makh;
+    }
+
+    public void setTenkh(String tenkh) {
+        this.tenkh = tenkh;
+    }
+
+    public void setDiachi(String diachi) {
+        this.diachi = diachi;
+    }
+
+    public void setSdt(String sdt) {
+        this.sdt = sdt;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getMakh() {
+        return makh;
+    }
+
+    public String getTenkh() {
+        return tenkh;
+    }
+
+    public String getDiachi() {
+        return diachi;
+    }
+
+    public String getSdt() {
+        return sdt;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setByInput(){
+        System.out.print("Nhap ma khach hang: ");
+        makh = Input.getString();
+        System.out.print("Nhap ten khach hang: ");
+        tenkh = Input.getString();
+        System.out.print("Nhap Dia chi: ");
+        diachi = Input.getString();
+        System.out.print("Nhap sdt: ");
+        sdt = Input.getString();
+        System.out.print("Email: ");
+        email = Input.getString();
+    }
+
+    @Override public String toString(){
+        System.out.println("Ma khach hang: " + makh);
+        System.out.println("Ten khach hang: " + tenkh);
+        System.out.println("Dia chi: " + diachi);
+        System.out.println("SDT: " + sdt);
+        System.out.println("Email: " + email);
+        return "";
+    }
+}
