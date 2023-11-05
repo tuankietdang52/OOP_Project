d80274756496d9b7b419975b33c7676989cf4951
Nguyễn Hùng Lê Trung <trungbekerteo001@gmail.com>
2023-11-05 14:29:25 +0700

    Sửa các hàm tìm kiếm của DSSP, thêm CustomerList

--- src/ProductContainer/DSSP.java
+++ src/ProductContainer/DSSP.java
@@ -7,23 +7,23 @@ public class DSSP {
     Scanner input = new Scanner(System.in);
     public void nhap() {
         System.out.print("Nhap so luong san pham: ");
-        this.n = input.nextInt();
-        this.ds = new Product[this.n];
-        for (int i = 0; i < this.n; ++i) {
+        n = input.nextInt();
+        ds = new Product[n];
+        for (int i = 0; i < n; ++i) {
             System.out.println("San pham thu " + (i + 1));
             System.out.print("Day la quan(0) hay ao(1)?: ");
             int opt = input.nextInt();
             if (opt == 0) {
-                this.ds[i] = new Pant();
+                ds[i] = new Pant();
             } else {
-                this.ds[i] = new Shirt();
+                ds[i] = new Shirt();
             }
-            this.ds[i].setByInput();
+            ds[i].setByInput();
         }
     }
 
     public String toString() {
-        for (int i = 0; i < this.n; ++i) {
+        for (int i = 0; i < n; ++i) {
             System.out.println("\nSan pham thu " + (i + 1) + ":");
             System.out.println(ds[i]);
         }
@@ -45,6 +45,7 @@ public class DSSP {
 
     public void them() {
         ds = Arrays.copyOf(ds, n + 1);
+        System.out.print("Quan(0) / ao(1)");
         int opt = input.nextInt();
         if (opt == 0) {
             ds[n] = new Pant();
@@ -71,7 +72,7 @@ public class DSSP {
         ds = Arrays.copyOf(ds, n + k);
         int i = 0;
         while (i < k) {
-            System.out.print("Day la quan hay ao?: ");
+            System.out.print("Day la quan(0) hay ao(1)?: ");
             int opt = input.nextInt();
             if (opt == 0) {
                 ds[n] = new Pant();
@@ -162,7 +163,7 @@ public class DSSP {
     }
     public int timkiemTensp(String tensp) {
         for (int i = 0; i < n; i++) {
-            if (ds[i].getTensp().contains(Xoa_khoang_trang_thua(tensp)))
+            if (ds[i].getTensp().contains(tensp))
                 return i;
         }
         return -1;
@@ -178,7 +179,7 @@ public class DSSP {
     }
     public Product timkiemTensp_Product(String tensp) {
         for (int i = 0; i < n; i++) {
-            if (ds[i].getTensp().contains(Xoa_khoang_trang_thua(tensp)))
+            if (ds[i].getTensp().contains(tensp))
                 return ds[i];
         }
         return null;
@@ -187,25 +188,39 @@ public class DSSP {
         System.out.print("Ten sp can tim: ");
         String tensp = input.nextLine();
         DSSP a = new DSSP();
+        a.ds=new Product[a.n];
         for (int i = 0; i < n; i++) {
-            if (ds[i].getTensp().contains(Xoa_khoang_trang_thua(tensp)))
+            if (ds[i].getTensp().contains(Xoa_khoang_trang_thua(tensp))) {
                 if (ds[i] instanceof Pant) {
-                    a.them((Pant) ds[i]);
-                } else {
-                    a.them((Shirt) ds[i]);
+                    a.ds=Arrays.copyOf(a.ds, a.n+1);
+                    a.ds[a.n]=new Pant((Pant) ds[i]);
+                    ++a.n;
                 }
+                else  {
+                    a.ds=Arrays.copyOf(a.ds, a.n+1);
+                    a.ds[a.n]=new Shirt((Shirt) ds[i]);
+                    ++a.n;
+                }
+            }
         }
         return a;
     }
     public DSSP timkiemTensp_DSSP(String tensp) {
         DSSP a = new DSSP();
+        a.ds=new Product[a.n];
         for (int i = 0; i < n; i++) {
-            if (ds[i].getTensp().contains(tensp))
+            if (ds[i].getTensp().contains(tensp)) {
                 if (ds[i] instanceof Pant) {
-                    a.them((Pant) ds[i]);
-                } else {
-                    a.them((Shirt) ds[i]);
+                    a.ds=Arrays.copyOf(a.ds, a.n+1);
+                    a.ds[a.n]=new Pant((Pant) ds[i]);
+                    ++a.n;
                 }
+                else  {
+                    a.ds=Arrays.copyOf(a.ds, a.n+1);
+                    a.ds[a.n]=new Shirt((Shirt) ds[i]);
+                    ++a.n;
+                }
+            }
         }
         return a;
     }
@@ -220,7 +235,7 @@ public class DSSP {
     }
     public int timkiemGioitinh(String gioitinh) {
         for (int i = 0; i < n; i++) {
-            if (ds[i].getGioitinh().contains(Xoa_khoang_trang_thua(gioitinh)))
+            if (ds[i].getGioitinh().contains(gioitinh))
                 return i;
         }
         return -1;
@@ -236,7 +251,7 @@ public class DSSP {
     }
     public Product timkiemGioitinh_Product(String gioitinh) {
         for (int i = 0; i < n; i++) {
-            if (ds[i].getGioitinh().contains(Xoa_khoang_trang_thua(gioitinh)))
+            if (ds[i].getGioitinh().contains(gioitinh))
                 return ds[i];
         }
         return null;
@@ -245,25 +260,39 @@ public class DSSP {
         System.out.print("Gioi tinh can tim: ");
         String gioitinh = input.nextLine();
         DSSP a = new DSSP();
+        a.ds=new Product[a.n];
         for (int i = 0; i < n; i++) {
-            if (ds[i].getGioitinh().contains(Xoa_khoang_trang_thua(gioitinh)))
+            if (ds[i].getGioitinh().contains(Xoa_khoang_trang_thua(gioitinh))) {
                 if (ds[i] instanceof Pant) {
-                    a.them((Pant) ds[i]);
-                } else {
-                    a.them((Shirt) ds[i]);
+                    a.ds=Arrays.copyOf(a.ds, a.n+1);
+                    a.ds[a.n]=new Pant((Pant) ds[i]);
+                    ++a.n;
                 }
+                else  {
+                    a.ds=Arrays.copyOf(a.ds, a.n+1);
+                    a.ds[a.n]=new Shirt((Shirt) ds[i]);
+                    ++a.n;
+                }
+            }
         }
         return a;
     }
     public DSSP timkiemGioitinh_DSSP(String gioitinh) {
         DSSP a = new DSSP();
+        a.ds=new Product[a.n];
         for (int i = 0; i < n; i++) {
-            if (ds[i].getGioitinh().contains(Xoa_khoang_trang_thua(gioitinh)))
+            if (ds[i].getGioitinh().contains(gioitinh)) {
                 if (ds[i] instanceof Pant) {
-                    a.them((Pant) ds[i]);
-                } else {
-                    a.them((Shirt) ds[i]);
+                    a.ds=Arrays.copyOf(a.ds, a.n+1);
+                    a.ds[a.n]=new Pant((Pant) ds[i]);
+                    ++a.n;
                 }
+                else  {
+                    a.ds=Arrays.copyOf(a.ds, a.n+1);
+                    a.ds[a.n]=new Shirt((Shirt) ds[i]);
+                    ++a.n;
+                }
+            }
         }
         return a;
     }
@@ -307,26 +336,40 @@ public class DSSP {
         System.out.print("Size can tim: ");
         String size = input.nextLine();
         DSSP a = new DSSP();
+        a.ds=new Product[a.n];
         for (int i = 0; i < n; i++) {
-            if (ds[i].getSize().contains(size))
+            if (ds[i].getSize().contains(size)) {
                 if (ds[i] instanceof Pant) {
-                    a.them((Pant) ds[i]);
-                } else {
-                    a.them((Shirt) ds[i]);
+                    a.ds=Arrays.copyOf(a.ds, a.n+1);
+                    a.ds[a.n]=new Pant((Pant) ds[i]);
+                    ++a.n;
                 }
+                else  {
+                    a.ds=Arrays.copyOf(a.ds, a.n+1);
+                    a.ds[a.n]=new Shirt((Shirt) ds[i]);
+                    ++a.n;
+                }
+            }
         }
         return a;
     }
 
     public DSSP timkiemSize_DSSP(String size) {
         DSSP a = new DSSP();
+        a.ds=new Product[a.n];
         for (int i = 0; i < n; i++) {
-            if (ds[i].getSize().contains(size))
+            if (ds[i].getSize().contains(size)) {
                 if (ds[i] instanceof Pant) {
-                    a.them((Pant) ds[i]);
-                } else {
-                    a.them((Shirt) ds[i]);
+                    a.ds=Arrays.copyOf(a.ds, a.n+1);
+                    a.ds[a.n]=new Pant((Pant) ds[i]);
+                    ++a.n;
                 }
+                else  {
+                    a.ds=Arrays.copyOf(a.ds, a.n+1);
+                    a.ds[a.n]=new Shirt((Shirt) ds[i]);
+                    ++a.n;
+                }
+            }
         }
         return a;
     }
@@ -335,7 +378,7 @@ public class DSSP {
         System.out.println("Chat lieu can tim: ");
         String chatlieu = input.nextLine();
         for (int i = 0; i < n; i++) {
-            if (ds[i].getChatlieu().contains(chatlieu))
+            if (ds[i].getChatlieu().contains(Xoa_khoang_trang_thua(chatlieu)))
                 System.out.print(i + " ");
         }
         System.out.println("\n");
@@ -353,7 +396,7 @@ public class DSSP {
         System.out.println("Chat lieu can tim: ");
         String chatlieu = input.nextLine();
         for (int i = 0; i < n; i++) {
-            if (ds[i].getChatlieu().contains(chatlieu))
+            if (ds[i].getChatlieu().contains(Xoa_khoang_trang_thua(chatlieu)))
                 System.out.print(ds[i] + "/n");
         }
         System.out.println("\n");
@@ -371,26 +414,40 @@ public class DSSP {
         System.out.print("Chat lieu can tim: ");
         String chatlieu = input.nextLine();
         DSSP a = new DSSP();
+        a.ds=new Product[a.n];
         for (int i = 0; i < n; i++) {
-            if (ds[i].getChatlieu().contains(chatlieu))
+            if (ds[i].getChatlieu().contains(Xoa_khoang_trang_thua(chatlieu))) {
                 if (ds[i] instanceof Pant) {
-                    a.them((Pant) ds[i]);
-                } else {
-                    a.them((Shirt) ds[i]);
+                    a.ds=Arrays.copyOf(a.ds, a.n+1);
+                    a.ds[a.n]=new Pant((Pant) ds[i]);
+                    ++a.n;
                 }
+                else  {
+                    a.ds=Arrays.copyOf(a.ds, a.n+1);
+                    a.ds[a.n]=new Shirt((Shirt) ds[i]);
+                    ++a.n;
+                }
+            }
         }
         return a;
     }
 
     public DSSP timkiemChatlieu_DSSP(String chatlieu) {
         DSSP a = new DSSP();
+        a.ds=new Product[a.n];
         for (int i = 0; i < n; i++) {
-            if (ds[i].getChatlieu().contains(chatlieu))
+            if (ds[i].getChatlieu().contains(chatlieu)) {
                 if (ds[i] instanceof Pant) {
-                    a.them((Pant) ds[i]);
-                } else {
-                    a.them((Shirt) ds[i]);
+                    a.ds=Arrays.copyOf(a.ds, a.n+1);
+                    a.ds[a.n]=new Pant((Pant) ds[i]);
+                    ++a.n;
                 }
+                else  {
+                    a.ds=Arrays.copyOf(a.ds, a.n+1);
+                    a.ds[a.n]=new Shirt((Shirt) ds[i]);
+                    ++a.n;
+                }
+            }
         }
         return a;
     }
@@ -434,27 +491,42 @@ public class DSSP {
     public DSSP timkiemDongia_DSSP() {
         System.out.print("Don gia can tim: ");
         double dongia = input.nextDouble();
+        input.nextLine();
         DSSP a = new DSSP();
+        a.ds=new Product[a.n];
         for (int i = 0; i < n; i++) {
-            if (Double.compare(ds[i].getDongia(), dongia) == 0)
+            if (Double.compare(ds[i].getDongia(), dongia) == 0) {
                 if (ds[i] instanceof Pant) {
-                    a.them((Pant) ds[i]);
-                } else {
-                    a.them((Shirt) ds[i]);
+                    a.ds=Arrays.copyOf(a.ds, a.n+1);
+                    a.ds[a.n]=new Pant((Pant) ds[i]);
+                    ++a.n;
                 }
+                else  {
+                    a.ds=Arrays.copyOf(a.ds, a.n+1);
+                    a.ds[a.n]=new Shirt((Shirt) ds[i]);
+                    ++a.n;
+                }
+            }
         }
         return a;
     }
 
     public DSSP timkiemDongia_DSSP(double dongia) {
         DSSP a = new DSSP();
+        a.ds=new Product[a.n];
         for (int i = 0; i < n; i++) {
-            if (Double.compare(ds[i].getDongia(), dongia) == 0)
+            if (Double.compare(ds[i].getDongia(), dongia) == 0) {
                 if (ds[i] instanceof Pant) {
-                    a.them((Pant) ds[i]);
-                } else {
-                    a.them((Shirt) ds[i]);
+                    a.ds=Arrays.copyOf(a.ds, a.n+1);
+                    a.ds[a.n]=new Pant((Pant) ds[i]);
+                    ++a.n;
                 }
+                else  {
+                    a.ds=Arrays.copyOf(a.ds, a.n+1);
+                    a.ds[a.n]=new Shirt((Shirt) ds[i]);
+                    ++a.n;
+                }
+            }
         }
         return a;
     }
@@ -507,10 +579,14 @@ public class DSSP {
         System.out.print("Tim ao co mu hay khong mu: ");
         boolean mutrumdau = input.nextBoolean();
         DSSP a = new DSSP();
+        a.ds=new Product[a.n];
         for (int i = 0; i < n; i++) {
             if (ds[i] instanceof Shirt) {
-                if (((Shirt) ds[i]).getMutrumdau() == mutrumdau)
-                    a.them((Shirt) ds[i]);
+                if (((Shirt) ds[i]).getMutrumdau() == mutrumdau) {
+                    a.ds = Arrays.copyOf(a.ds, a.n + 1);
+                    a.ds[a.n] = new Shirt((Shirt) ds[i]);
+                    ++a.n;
+                }
             }
         }
         return a;
@@ -518,10 +594,14 @@ public class DSSP {
 
     public DSSP timkiemMutrumdau_DSSP(boolean mutrumdau) {
         DSSP a = new DSSP();
+        a.ds=new Product[a.n];
         for (int i = 0; i < n; i++) {
             if (ds[i] instanceof Shirt) {
-                if (((Shirt) ds[i]).getMutrumdau() == mutrumdau)
-                    a.them((Shirt) ds[i]);
+                if (((Shirt) ds[i]).getMutrumdau() == mutrumdau) {
+                    a.ds = Arrays.copyOf(a.ds, a.n + 1);
+                    a.ds[a.n] = new Shirt((Shirt) ds[i]);
+                    ++a.n;
+                }
             }
         }
         return a;
@@ -576,10 +656,14 @@ public class DSSP {
         System.out.print("Tim quan co thun hay khong thun: ");
         boolean thunquan = input.nextBoolean();
         DSSP a = new DSSP();
+        a.ds=new Product[a.n];
         for (int i = 0; i < n; i++) {
             if (ds[i] instanceof Pant) {
-                if (((Pant) ds[i]).getThunquan() == thunquan)
-                    a.them((Pant) ds[i]);
+                if (((Pant) ds[i]).getThunquan() == thunquan) {
+                    a.ds = Arrays.copyOf(a.ds, a.n + 1);
+                    a.ds[a.n] = new Shirt((Shirt) ds[i]);
+                    ++a.n;
+                }
             }
         }
         return a;
@@ -587,10 +671,14 @@ public class DSSP {
 
     public DSSP timkiemThunquan_DSSP(boolean thunquan) {
         DSSP a = new DSSP();
+        a.ds=new Product[a.n];
         for (int i = 0; i < n; i++) {
             if (ds[i] instanceof Pant) {
-                if (((Pant) ds[i]).getThunquan() == thunquan)
-                    a.them((Pant) ds[i]);
+                if (((Pant) ds[i]).getThunquan() == thunquan) {
+                    a.ds = Arrays.copyOf(a.ds, a.n + 1);
+                    a.ds[a.n] = new Shirt((Shirt) ds[i]);
+                    ++a.n;
+                }
             }
         }
         return a;
@@ -607,7 +695,7 @@ public class DSSP {
 
     public int timkiemHoatietu(String hoatiet) {
         for (int i = 0; i < n; i++) {
-            if (((Shirt) ds[i]).getHoatiet().contains(Xoa_khoang_trang_thua(hoatiet)))
+            if (((Shirt) ds[i]).getHoatiet().contains(hoatiet))
                 return i;
         }
         return -1;
@@ -625,7 +713,7 @@ public class DSSP {
 
     public Product timkiemHoatiet_Product(String hoatiet) {
         for (int i = 0; i < n; i++) {
-            if (((Shirt) ds[i]).getHoatiet().contains(Xoa_khoang_trang_thua(hoatiet)))
+            if (((Shirt) ds[i]).getHoatiet().contains(hoatiet))
                 return ds[i];
         }
         return null;
@@ -635,10 +723,14 @@ public class DSSP {
         System.out.print("Hoa tiet can tim: ");
         String hoatiet = input.nextLine();
         DSSP a = new DSSP();
+        a.ds=new Product[a.n];
         for (int i = 0; i < n; i++) {
-            if (((Shirt) ds[i]).getHoatiet().contains(Xoa_khoang_trang_thua(hoatiet))) {
-                if (ds[i] instanceof Shirt)
-                    a.them((Shirt) ds[i]);
+            if (ds[i] instanceof Shirt) {
+                if (((Shirt) ds[i]).getHoatiet().contains(hoatiet)) {
+                    a.ds = Arrays.copyOf(a.ds, a.n + 1);
+                    a.ds[a.n] = new Shirt((Shirt) ds[i]);
+                    ++a.n;
+                }
             }
         }
         return a;
@@ -646,10 +738,14 @@ public class DSSP {
 
     public DSSP timkiemHoatiet_DSSP(String hoatiet) {
         DSSP a = new DSSP();
+        a.ds=new Product[a.n];
         for (int i = 0; i < n; i++) {
-            if (((Shirt) ds[i]).getHoatiet().contains(Xoa_khoang_trang_thua(hoatiet))) {
-                if (ds[i] instanceof Shirt)
-                    a.them((Shirt) ds[i]);
+            if (ds[i] instanceof Shirt) {
+                if (((Shirt) ds[i]).getHoatiet().contains(hoatiet)) {
+                    a.ds = Arrays.copyOf(a.ds, a.n + 1);
+                    a.ds[a.n] = new Shirt((Shirt) ds[i]);
+                    ++a.n;
+                }
             }
         }
         return a;
@@ -789,10 +885,10 @@ public class DSSP {
                 }
             }}
         else {
-            System.out.println("Ma sp ko ton tai!");
+            System.out.println("Ma sp khong ton tai!");
         }}
     public void sua() {
-        System.out.print("Nhap ma cua sp muon sua: ");
+        System.out.print("Nhap ma cua sp muon sua thong tin: ");
         String masp=input.nextLine();
         int flag=0;
         int opt;
@@ -940,7 +1036,7 @@ public class DSSP {
                 }
             }}
         else {
-            System.out.println("Ma sp ko ton tai!");
+            System.out.println("Ma sp khong ton tai!");
         }
     }
 }
